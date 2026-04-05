9db7b80b64c954abd8fb31b53f65b27d8efa8e7f
Valeriy <tamvalera@gmail.com>
2026-04-05 13:30:15 +0700

    Intermediate Commit

---
+++ apps/server/src/modules/reviews/review.middleware.ts
@@ -0,0 +1,43 @@
+import { Response, NextFunction } from "express";
+import { prisma } from "@repo/database";
+import { ReviewModel } from "./review.model.js";
+import { AuthRequest } from "../../shared/middlewares/auth.middleware.js";
+import * as fs from "node:fs";
+
+export const validateReviewAccess = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { orderId } = req.body;
+
+    // 🎯 Функция для удаления файлов, если проверка не пройдена
+    const cleanup = () => {
+      if (req.files) {
+        (req.files as Express.Multer.File[]).forEach((file) => {
+          if (fs.existsSync(file.path)) fs.unlinkSync(file.path);
+        });
+      }
+    };
+
+    const order = await prisma.order.findUnique({
+      where: { id: orderId, userId: req.user.id, status: "COMPLETED" },
+    });
+
+    if (!order) {
+      cleanup(); //Стираем файлы
+      return res.status(403).json({ message: "Заказ не завершен или не ваш" });
+    }
+
+    const existingReview = await ReviewModel.findOne({ orderId });
+    if (existingReview) {
+      cleanup(); //Стираем файлы
+      return res.status(400).json({ message: "Отзыв уже существует" });
+    }
+
+    next();
+  } catch (e) {
+    next(e);
+  }
+};
--- apps/server/src/modules/reviews/review.routes.ts
+++ apps/server/src/modules/reviews/review.routes.ts
@@ -2,15 +2,17 @@ import { Router } from "express";
 import * as reviewController from "./review.controller.js";
 import { authMiddleware } from "src/shared/middlewares/auth.middleware.js";
 import { uploadReviewImages } from "src/lib/storage.js";
+import { validateReviewAccess } from "./review.middleware.js";
 
 const router = Router();
 
 //Создание отзыва:
 router.post(
   "/",
-  authMiddleware,
-  uploadReviewImages,
-  reviewController.createReview,
+  authMiddleware, //Проверяем авторизацию
+  uploadReviewImages, //Загружаем изображения. Multer заполнит req.body
+  validateReviewAccess, //Проверяем, что отзыв ещё не оставлялся
+  reviewController.createReview, //Оставляем отзыв
 );
 //Получить все отзывы для конкретной модели мотоцикла:
 router.get("/:motorcycleId", reviewController.getMotorcycleReviews);
