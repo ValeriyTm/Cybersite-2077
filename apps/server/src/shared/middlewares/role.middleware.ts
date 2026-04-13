37afc63d71aa3411b5a71457f3aa4c2bc2539f2e
Valeriy <tamvalera@gmail.com>
2026-04-13 11:29:45 +0700

    Intermediate Commit

--- apps/server/src/shared/middlewares/role.middleware.ts
+++ apps/server/src/shared/middlewares/role.middleware.ts
@@ -4,19 +4,43 @@ import { Response, NextFunction } from "express";
 import { AuthRequest } from "./auth.middleware.js";
 //Используем свой класс для выбрасывания ошибок:
 import { AppError } from "../utils/app-error.js";
+import { prisma } from "@repo/database";
 
 //При вызове функции в её аргумент помещаем массив разрешенных ролей:
 export const roleMiddleware = (allowedRoles: string[]) => {
-  return (req: AuthRequest, res: Response, next: NextFunction) => {
+  return async (req: AuthRequest, res: Response, next: NextFunction) => {
     // 1. Проверяем, авторизован ли юзер вообще:
     if (!req.user) {
       throw new AppError(401, "Пользователь не авторизован");
     }
 
-    // 2. Проверяем, входит ли роль юзера в список разрешенных:
+    const { id, role } = req.user;
+
+    // 2. Проверяем, входит ли роль юзера в список разрешенных (берем данные из токена юзера):
     //(проверяем установленные роли и роль пользователя)
-    if (!allowedRoles.includes(req.user.role)) {
+    if (!allowedRoles.includes(role)) {
       throw new AppError(403, "У вас недостаточно прав для этого действия");
+    }
+
+    // 3.Дополнительная проверка в БД для админов:
+    if (role === "ADMIN" || role === "SUPERADMIN") {
+      // Если юзер заявляет, что он админ — идем проверять это в PostgreSQL
+      const dbUser = await prisma.user.findUnique({
+        where: { id },
+        select: { role: true, isActivated: true },
+      });
+
+      //Если юзера нет, он не активирован или его роль в уже БД изменилась (например, отозвали, а в access token роль старая):
+      if (!dbUser || !["ADMIN", "SUPERADMIN"].includes(dbUser.role)) {
+        return next(
+          new AppError(403, "Подтверждение прав администратора не удалось"),
+        );
+      }
+
+      // Дополнительно можно проверить статус активации
+      if (!dbUser.isActivated) {
+        return next(new AppError(403, "Аккаунт администратора не активирован"));
+      }
     }
 
     next();
