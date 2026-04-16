64d805b35b7eece541c270ee6eeb59210dc336f3
Valeriy <tamvalera@gmail.com>
2026-04-16 19:47:52 +0700

    Intermediate Commit

--- apps/web/src/widgets/Header/ui/Header.tsx
+++ apps/web/src/widgets/Header/ui/Header.tsx
@@ -156,27 +156,29 @@ export const Header = () => {
       {/*1)Верхняя часть: Ссылки */}
       <div className={styles.topLine}>
         <div className={styles.container}>
-          {/*1.1)Навбар:*/}
-          <nav className={styles.topNav}>
-            <Link to="/">Главная</Link>
-            <Link to="/about">О компании</Link>
-            {/* <Link to="/shipping">Доставка и оплата</Link> */}
-            <Link to="/contacts">Контакты</Link>
-            <Link to="/news">Новости</Link>
-            <Link to="/promos">Промокоды</Link>
-            <Link to="/support">Поддержка</Link>
-            {canSee && <Link to="/admin">Управление</Link>}
-          </nav>
-          {/*1.2)Смена темы:*/}
-          <div className={styles.themeSwitcher}>
-            {themes.map((t) => (
-              <img
-                key={t.id}
-                src={t.img}
-                onClick={() => setTheme(t.id as any)}
-                className={theme === t.id ? styles.active : styles.inactive}
-              />
-            ))}
+          <div className={styles.topWrapper}>
+            {/*1.1)Навбар:*/}
+            <nav className={styles.topNav}>
+              <Link to="/">Главная</Link>
+              <Link to="/about">О компании</Link>
+              {/* <Link to="/shipping">Доставка и оплата</Link> */}
+              <Link to="/contacts">Контакты</Link>
+              <Link to="/news">Новости</Link>
+              <Link to="/promos">Промокоды</Link>
+              <Link to="/support">Поддержка</Link>
+              {canSee && <Link to="/admin">Управление</Link>}
+            </nav>
+            {/*1.2)Смена темы:*/}
+            <div className={styles.themeSwitcher}>
+              {themes.map((t) => (
+                <img
+                  key={t.id}
+                  src={t.img}
+                  onClick={() => setTheme(t.id as any)}
+                  className={theme === t.id ? styles.active : styles.inactive}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
@@ -184,215 +186,222 @@ export const Header = () => {
       {/*2)Нижняя часть: Основные инструменты */}
       <div className={styles.mainLine}>
         <div className={styles.container}>
-          {/*2.1)Логотип*/}
-          <Link to="/" className={styles.logolink}>
-            <img src={logoUrl} alt="Main Logo" className={styles.logo} />
-          </Link>
+          <div className={styles.mainWrapper}>
+            <div className={styles.leftBotMenu}>
 
-          {/*2.2)Кнопка каталога с Hover-меню*/}
-          <div
-            className={styles.catalogWrapper}
-            onMouseEnter={() => setIsCatalogOpen(true)}
-            onMouseLeave={() => setIsCatalogOpen(false)}
-          >
-            <Link to="/catalog/motorcycles" className={styles.catalogBtn}>
-              <span className={styles.burger}>☰</span> Каталог
-            </Link>
+              {/*2.1)Логотип*/}
+              <Link to="/" className={styles.logolink}>
+                <img src={logoUrl} alt="Main Logo" className={styles.logo} />
+              </Link>
 
-            {/*Статическое выпадающее меню */}
-            {isCatalogOpen && (
-              <div className={styles.dropdown}>
-                <div className={styles.dropdownContent}>
-                  {/*Левая часть выпадающего меню: Группы товаров*/}
-                  <aside className={styles.sideNav}>
-                    <div
-                      className={`${styles.sideItem} ${activeMainCat === "moto" ? styles.activeSide : ""}`}
-                      onMouseEnter={() => setActiveMainCat("moto")}
-                    >
-                      <img
-                        src="http://localhost:3001/static/icons/moto-icon.png"
-                        alt="motorcycle icon"
-                      />
-                      <span>Мототехника</span>
-                      <span className={styles.arrow}>›</span>
-                    </div>
+              {/*2.2)Кнопка каталога с Hover-меню*/}
+              <div
+                className={styles.catalogWrapper}
+                onMouseEnter={() => setIsCatalogOpen(true)}
+                onMouseLeave={() => setIsCatalogOpen(false)}
+              >
+                <Link to="/catalog/motorcycles" className={styles.catalogBtn}>
+                  <span className={styles.burger}>☰</span> Каталог
+                </Link>
 
-                    <div className={`${styles.sideItem} ${styles.disabled}`}>
-                      <img
-                        src="http://localhost:3001/static/icons/equip-icon.png"
-                        alt="motorcycle equipment icon"
-                      />
-                      <span>Экипировка</span>
-                      <span className={styles.arrow}>›</span>
-                    </div>
+                {/*Статическое выпадающее меню */}
+                {isCatalogOpen && (
+                  <div className={styles.dropdown}>
+                    <div className={styles.dropdownContent}>
+                      {/*Левая часть выпадающего меню: Группы товаров*/}
+                      <aside className={styles.sideNav}>
+                        <div
+                          className={`${styles.sideItem} ${activeMainCat === "moto" ? styles.activeSide : ""}`}
+                          onMouseEnter={() => setActiveMainCat("moto")}
+                        >
+                          <img
+                            src="http://localhost:3001/static/icons/moto-icon.png"
+                            alt="motorcycle icon"
+                          />
+                          <span>Мототехника</span>
+                          <span className={styles.arrow}>›</span>
+                        </div>
 
-                    <div className={`${styles.sideItem} ${styles.disabled}`}>
-                      <img
-                        src="http://localhost:3001/static/icons/gear-icon.png"
-                        alt="gear icon"
-                      />
-                      <span>Запчасти</span>
-                      <span className={styles.arrow}>›</span>
-                    </div>
-                  </aside>
+                        <div className={`${styles.sideItem} ${styles.disabled}`}>
+                          <img
+                            src="http://localhost:3001/static/icons/equip-icon.png"
+                            alt="motorcycle equipment icon"
+                          />
+                          <span>Экипировка</span>
+                          <span className={styles.arrow}>›</span>
+                        </div>
 
-                  {/*Правая часть ыпаадающего меню: бренды*/}
-                  <section className={styles.mainPanel}>
-                    {activeMainCat === "moto" ? (
-                      <div className={styles.brandsGrid}>
-                        {TOP_BRANDS.map((brand) => {
-                          const motoLink = `http://localhost:3001/static/moto_brands/${brand.slug}.png`;
-                          return (
+                        <div className={`${styles.sideItem} ${styles.disabled}`}>
+                          <img
+                            src="http://localhost:3001/static/icons/gear-icon.png"
+                            alt="gear icon"
+                          />
+                          <span>Запчасти</span>
+                          <span className={styles.arrow}>›</span>
+                        </div>
+                      </aside>
+
+                      {/*Правая часть ыпаадающего меню: бренды*/}
+                      <section className={styles.mainPanel}>
+                        {activeMainCat === "moto" ? (
+                          <div className={styles.brandsGrid}>
+                            {TOP_BRANDS.map((brand) => {
+                              const motoLink = `http://localhost:3001/static/moto_brands/${brand.slug}.png`;
+                              return (
+                                <Link
+                                  key={brand.slug}
+                                  to={`/catalog/motorcycles/${brand.slug}`}
+                                  className={styles.brandItem}
+                                  onClick={() => setIsCatalogOpen(false)}
+                                >
+                                  <div className={styles.brandIcon}>
+                                    <img
+                                      src={motoLink}
+                                      alt="moto preview"
+                                      className={styles.motoIcon}
+                                    />
+                                  </div>
+                                  <span>
+                                    Мотоциклы <strong>{brand.name}</strong>
+                                  </span>
+                                </Link>
+                              );
+                            })}
+
+                            {/*Кнопка "Прочие бренды" */}
                             <Link
-                              key={brand.slug}
-                              to={`/catalog/motorcycles/${brand.slug}`}
+                              to="/catalog/motorcycles"
                               className={styles.brandItem}
                               onClick={() => setIsCatalogOpen(false)}
                             >
                               <div className={styles.brandIcon}>
                                 <img
-                                  src={motoLink}
-                                  alt="moto preview"
-                                  className={styles.motoIcon}
+                                  src="http://localhost:3001/static/moto_brands/scooter.png"
+                                  alt="default icon"
                                 />
                               </div>
-                              <span>
-                                Мотоциклы <strong>{brand.name}</strong>
-                              </span>
+                              <span>Прочие бренды</span>
                             </Link>
-                          );
-                        })}
-
-                        {/*Кнопка "Прочие бренды" */}
-                        <Link
-                          to="/catalog/motorcycles"
-                          className={styles.brandItem}
-                          onClick={() => setIsCatalogOpen(false)}
-                        >
-                          <div className={styles.brandIcon}>
-                            <img
-                              src="http://localhost:3001/static/moto_brands/scooter.png"
-                              alt="default icon"
-                            />
                           </div>
-                          <span>Прочие бренды</span>
-                        </Link>
-                      </div>
-                    ) : (
-                      <div className={styles.emptyPanel}>
-                        Скоро в продаже...
-                      </div>
-                    )}
-                  </section>
-                </div>
-              </div>
-            )}
-          </div>
-
-          {/*2.3)Поиск с подсказками (Autocomplete) */}
-          <form
-            className={styles.searchBox}
-            onSubmit={handleSearchSubmit}
-            ref={searchRef}
-          >
-            <input
-              type="text"
-              value={searchQuery}
-              onChange={handleInputChange}
-              onKeyDown={(e) => e.key === "Enter" && handleSearchSubmit()}
-              placeholder="Поиск по каталогу"
-            />
-            <button type="submit">Найти</button>
-
-            {suggestions.length > 0 && (
-              <div className={styles.suggestions}>
-                {suggestions.map((moto) => {
-                  const linkToImage = moto.mainImage
-                    ? `http://localhost:3001/static/motorcycles/${moto.mainImage}`
-                    : "http://localhost:3001/static/defaults/default-card-icon.jpg";
-                  return (
-                    <Link
-                      key={moto.id}
-                      to={`/catalog/motorcycles/${moto.brandSlug}/${moto.slug}`}
-                      className={styles.suggestItem}
-                      onClick={() => {
-                        setSuggestions([]);
-                        setSearchQuery("");
-                      }}
-                    >
-                      <div className={styles.suggestImg}>
-                        <img src={linkToImage} alt="" />
-                      </div>
-                      <div className={styles.suggestInfo}>
-                        <span className={styles.suggestModel}>
-                          {moto.model}
-                        </span>
-                        <span className={styles.suggestYear}>
-                          {moto.year} г.
-                        </span>
-                      </div>
-                    </Link>
-                  );
-                })}
-              </div>
-            )}
-          </form>
-
-          {/*2.4)Блок пользователя:*/}
-          <div className={styles.userActions}>
-            <Link
-              to={isAuth ? "/profile" : "/auth"}
-              className={styles.profileLink}
-            >
-              <Avatar
-                src={isAuth ? avatarSrc : null}
-                alt={user?.name || "Гость"}
-                size="sm"
-                isAvatarLoading={isLoading} //Показываем спиннер, пока идет /refresh
-              />
-
-              <div className={styles.userInfo} title={user?.name || ''}>
-                <span className={styles.userName}>
-                  {/* Если авторизован и не грузится — имя, иначе "Войти" */}
-                  {isAuth && user && !isLoading ? user.name : "Войти"}
-                </span>
-              </div>
-            </Link>
-          </div>
-
-          {/*2.5)Блок заказов:*/}
-          <div className={styles.userOrders}>
-            {/*Кнопка избранного со счетчиком: */}
-            <Link
-              to="/profile/favorites"
-              className={styles.iconBtn}
-              title="Избранное"
-            >
-              ❤️
-              {favoritesCount > 0 && (
-                <span className={styles.counter}>{favoritesCount}</span>
-              )}
-            </Link>
-
-            {/*Кнопка корзины со счетчиком:*/}
-            <Link to="/cart" title="Корзина">
-              <button className={styles.iconBtn} title="Корзина">
-                🛒{" "}
-                {cartCount > 0 && (
-                  <span className={styles.counter}>{cartCount}</span>
+                        ) : (
+                          <div className={styles.emptyPanel}>
+                            Скоро в продаже...
+                          </div>
+                        )}
+                      </section>
+                    </div>
+                  </div>
                 )}
-              </button>
-            </Link>
+              </div>
 
-            {/*Кнопка заказов со счетчиком:*/}
-            <Link to="/orders/my" className={styles.iconBtn} title="Мои заказы">
-              📦
-              {activeOrdersCount > 0 && (
-                <span className={`${styles.counter} ${styles.orderCounter}`}>
-                  {activeOrdersCount}
-                </span>
-              )}
-            </Link>
+              {/*2.3)Поиск с подсказками (Autocomplete) */}
+              <form
+                className={styles.searchBox}
+                onSubmit={handleSearchSubmit}
+                ref={searchRef}
+              >
+                <input
+                  type="text"
+                  value={searchQuery}
+                  onChange={handleInputChange}
+                  onKeyDown={(e) => e.key === "Enter" && handleSearchSubmit()}
+                  placeholder="Поиск по каталогу"
+                />
+                <button type="submit">Найти</button>
+
+                {suggestions.length > 0 && (
+                  <div className={styles.suggestions}>
+                    {suggestions.map((moto) => {
+                      const linkToImage = moto.mainImage
+                        ? `http://localhost:3001/static/motorcycles/${moto.mainImage}`
+                        : "http://localhost:3001/static/defaults/default-card-icon.jpg";
+                      return (
+                        <Link
+                          key={moto.id}
+                          to={`/catalog/motorcycles/${moto.brandSlug}/${moto.slug}`}
+                          className={styles.suggestItem}
+                          onClick={() => {
+                            setSuggestions([]);
+                            setSearchQuery("");
+                          }}
+                        >
+                          <div className={styles.suggestImg}>
+                            <img src={linkToImage} alt="" />
+                          </div>
+                          <div className={styles.suggestInfo}>
+                            <span className={styles.suggestModel}>
+                              {moto.model}
+                            </span>
+                            <span className={styles.suggestYear}>
+                              {moto.year} г.
+                            </span>
+                          </div>
+                        </Link>
+                      );
+                    })}
+                  </div>
+                )}
+              </form>
+            </div>
+
+            <div className={styles.rightBotMenu}>
+              {/*2.4)Блок пользователя:*/}
+              <div className={styles.userActions}>
+                <Link
+                  to={isAuth ? "/profile" : "/auth"}
+                  className={styles.profileLink}
+                >
+                  <Avatar
+                    src={isAuth ? avatarSrc : null}
+                    alt={user?.name || "Гость"}
+                    size="sm"
+                    isAvatarLoading={isLoading} //Показываем спиннер, пока идет /refresh
+                  />
+
+                  <div className={styles.userInfo} title={user?.name || ''}>
+                    <span className={styles.userName}>
+                      {/* Если авторизован и не грузится — имя, иначе "Войти" */}
+                      {isAuth && user && !isLoading ? user.name : "Войти"}
+                    </span>
+                  </div>
+                </Link>
+              </div>
+
+              {/*2.5)Блок заказов:*/}
+              <div className={styles.userOrders}>
+                {/*Кнопка избранного со счетчиком: */}
+                <Link
+                  to="/profile/favorites"
+                  className={styles.iconBtn}
+                  title="Избранное"
+                >
+                  ❤️
+                  {favoritesCount > 0 && (
+                    <span className={styles.counter}>{favoritesCount}</span>
+                  )}
+                </Link>
+
+                {/*Кнопка корзины со счетчиком:*/}
+                <Link to="/cart" title="Корзина">
+                  <button className={styles.iconBtn} title="Корзина">
+                    🛒{" "}
+                    {cartCount > 0 && (
+                      <span className={styles.counter}>{cartCount}</span>
+                    )}
+                  </button>
+                </Link>
+
+                {/*Кнопка заказов со счетчиком:*/}
+                <Link to="/orders/my" className={styles.iconBtn} title="Мои заказы">
+                  📦
+                  {activeOrdersCount > 0 && (
+                    <span className={`${styles.counter} ${styles.orderCounter}`}>
+                      {activeOrdersCount}
+                    </span>
+                  )}
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
