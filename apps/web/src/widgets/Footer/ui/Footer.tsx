d5b27eaa99f54f8c942f7caddef5088b7aed18d6
Valeriy <tamvalera@gmail.com>
2026-04-17 16:07:39 +0700

    Intermediate Commit

--- apps/web/src/widgets/Footer/ui/Footer.tsx
+++ apps/web/src/widgets/Footer/ui/Footer.tsx
@@ -36,79 +36,81 @@ export const Footer = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.top}>
-        {/*Логотип*/}
-        <Link to="/" className={styles.logolink}>
-          <img src={logoUrl} alt="Main Logo" className={styles.logo} />
-        </Link>
+      <div className={styles.container}>
+        <div className={styles.top}>
+          {/*Логотип*/}
+          <Link to="/" className={styles.logolink}>
+            <img src={logoUrl} alt="Main Logo" className={styles.logo} />
+          </Link>
 
 
-        {/* Навигация */}
-        <div className={styles.column}>
-          <h3>Каталог</h3>
-          <ul>
-            <li>
-              <Link to="/catalog/motorcycles">Бренды</Link>
-            </li>
-            <li>
-              <Link to="">Мотоэкипировка</Link>
-            </li>
-            <li>
-              <Link to="">Запчасти и расходники</Link>
-            </li>
-          </ul>
-        </div>
+          {/* Навигация */}
+          <div className={styles.column}>
+            <h3>Каталог</h3>
+            <ul>
+              <li>
+                <Link to="/catalog/motorcycles">Бренды</Link>
+              </li>
+              <li>
+                <Link to="">Мотоэкипировка</Link>
+              </li>
+              <li>
+                <Link to="">Запчасти и расходники на любые модели</Link>
+              </li>
+            </ul>
+          </div>
 
-        <div className={styles.column}>
-          <h3>Информация</h3>
-          <ul>
-            <li>
-              <Link to="/terms">Согласие на обработку данных</Link>
-            </li>
-            <li>
-              <Link to="/privacy">Политика конфиденциальности</Link>
-            </li>
-          </ul>
-        </div>
+          <div className={styles.column}>
+            <h3>Информация</h3>
+            <ul>
+              <li>
+                <Link to="/terms">Согласие на обработку данных</Link>
+              </li>
+              <li>
+                <Link to="/privacy">Политика конфиденциальности</Link>
+              </li>
+            </ul>
+          </div>
 
-        <div className={styles.column}>
-          <h3>Компания</h3>
-          <ul>
-            <li>
-              <Link to="/contacts">Контакты</Link>
-            </li>
-            <li>
-              <Link to="/about">О нас</Link>
-            </li>
-          </ul>
-          <div className={styles.contacts}>
-            <a href="tel:+79830000000">+7(000) 000 00-00</a> <br></br>
-            <a href="mailto:info@cybersite2077.com">info@cybersite2077.com</a>
+          <div className={styles.column}>
+            <h3>Компания</h3>
+            <ul>
+              <li>
+                <Link to="/contacts">Контакты</Link>
+              </li>
+              <li>
+                <Link to="/about">О нас</Link>
+              </li>
+            </ul>
+            <div className={styles.contacts}>
+              <a href="tel:+79830000000">+7(000) 000 00-00</a>
+              <a href="mailto:info@cybersite2077.com">info@cybersite2077.com</a>
+            </div>
+          </div>
+
+          {/* Кнопка поддержки */}
+          <div className={styles.supportColumn}>
+            <h3>Остались вопросы?</h3>
+            <p>Задайте их нам прямо сейчас!</p>
+            <Link to="/support" className={styles.supportBtn}>
+              Задать вопрос
+            </Link>
           </div>
         </div>
 
-        {/* Кнопка поддержки */}
-        <div className={styles.supportColumn}>
-          <h3>Остались вопросы?</h3>
-          <p>Задайте их нам прямо сейчас!</p>
-          <Link to="/support" className={styles.supportBtn}>
-            Задать вопрос
-          </Link>
+        {/* Иконки соцсетей */}
+        <div className={styles.soc1als}>
+          <FaFacebook className={styles.facebook} /> <FaTelegram className={styles.telegram} /> <FaWhatsapp className={styles.whatsapp} /> <FaViber className={styles.viber} />
+          <FaTwitter className={styles.twitter} /> <FaTiktok className={styles.tiktok} /> <FaVk className={styles.vk} /> <FaYoutube className={styles.youtube} />
         </div>
-      </div>
 
-      {/* Иконки соцсетей */}
-      <div className={styles.soc1als}>
-        <FaFacebook className={styles.facebook} /> <FaTelegram className={styles.telegram} /> <FaWhatsapp className={styles.whatsapp} /> <FaViber className={styles.viber} />
-        <FaTwitter className={styles.twitter} /> <FaTiktok className={styles.tiktok} /> <FaVk className={styles.vk} /> <FaYoutube className={styles.youtube} />
-      </div>
+        <hr className={styles.divider} />
 
-      <hr className={styles.divider} />
-
-      {/* Нижняя часть */}
-      <div className={styles.bottom}>
-        <span>©{currentYear} Cybersite-2077 &nbsp;&nbsp;&nbsp;&nbsp;|</span>
-        <Link to="/agreement">Пользовательское соглашение</Link>
+        {/* Нижняя часть */}
+        <div className={styles.bottom}>
+          <span>©{currentYear} Cybersite-2077 &nbsp;&nbsp;&nbsp;&nbsp;|</span>
+          <Link to="/agreement">Пользовательское соглашение</Link>
+        </div>
       </div>
     </footer>
   );
